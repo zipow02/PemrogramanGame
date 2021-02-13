46f7c3ce60a583b5b0033a30532e3944cd344029
zipow02 <zivahanifa461@gmail.com>
2021-02-13 14:56:11 +0700

    comis
    comis

---
+++ FINA/Assets/Scripts/scene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene : MonoBehaviour {
+
+	// pindah scene ke menu
+	public void Menuscene() {
+		SceneManager.LoadScene ("Menu");
+		Debug.Log ("MenuPrompted");
+	}
+
+	// pindah scene ke gameplay
+	public void Gameplayscene() {
+		SceneManager.LoadScene ("Gameplay");
+		Debug.Log ("GameplayPrompted");
+	}
+
+	// keluar dari game
+	public void Gameoverscene () {
+		SceneManager.LoadScene ("Gameover");
+	Application.Quit ();
+
+	}
+}
